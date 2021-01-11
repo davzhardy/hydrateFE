e7616ff46f36a05c5686fb99ddbf734e51b05eb6
David Hardy <david.hardy50@gmail.com>
2021-01-11 11:58:00 +0100

    feat: global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React, { Fragment, Suspense, lazy } from "react";
+import React, { Suspense, lazy } from "react";
+import GlobalStyles from "./GlobalStyles";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from "./theme";
+
+const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
+
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoggedInComponent />
+      </Suspense>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/logged_in/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      Hello 2
+    </div>
+  );
+}
+
+export default Main;
