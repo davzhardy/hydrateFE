fbf99fee67f0be590413a527768dfb5ef7cf6c87
David Hardy <david.hardy50@gmail.com>
2021-02-01 22:20:42 +0100

    generate csv file of data

--- src/logged_in/components/datavisualisation/meal/HeaderIcons.js
+++ src/logged_in/components/datavisualisation/meal/HeaderIcons.js
@@ -5,6 +5,7 @@ import {
 } from '@material-ui/core'
 import DownloadIcon from "@material-ui/icons/CloudDownload";
 import SearchIcon from '@material-ui/icons/Search';
+import { useQueryClient } from "react-query";
 
 function HeaderIcons(props) {
 
@@ -17,18 +18,47 @@ function HeaderIcons(props) {
     setMealValue,
     setTime
   } = props
+
+  const queryClient = useQueryClient()
+  const mealsData = queryClient.getQueryData('meals').data.getAllMeals
+
+  const dataToCsv = data => {
+    const csvRows =[];
+    const headers = Object.keys(data[0]);
+    csvRows.push(headers.join(','));
+
+    for (const row of data) {
+      const values = headers.map(header => {
+        const escaped = (''+row[header]).replace(/"/g, '\\"');
+        return `"${escaped}"`
+      })
+      csvRows.push(values.join(','));
+    }
+
+    return csvRows.join('\n')
+  }
+
+  const download = data => {
+    const blob = new Blob([data, {type: 'text/csv'}])
+    const url = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.setAttribute('hidden', '')
+    a.setAttribute('href', url)
+    a.setAttribute('download', 'yourData.csv')
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+  }
+
+  const csvData = dataToCsv(mealsData)
   
   return (
     <Box display="flex" justifyContent="flex-end">
       <IconButton
         onClick={() => {
-          setSelectedRow(row)
-          setDescription(row.description)
-          setMealValue(row.meal)
-          setTime(row.time)
-          openRowModificationDialog()
+          download(csvData)
         }}
-        aria-label="Modify"
+        aria-label="Download"
       >
         <DownloadIcon />
       </IconButton>
@@ -40,7 +70,7 @@ function HeaderIcons(props) {
           setTime(row.time)
           openRowDeletionDialog()
         }}
-        aria-label="Delete"
+        aria-label="Search"
       >
         <SearchIcon  />
       </IconButton>
