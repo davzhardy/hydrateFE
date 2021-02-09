3926056eec6eb6c8a240d72d34cb22999a903c89
David Hardy <david.hardy50@gmail.com>
2021-02-09 14:48:13 +0100

    almost finalised meals graphs

--- src/logged_in/components/graphs/PackingGraph.js
+++ src/logged_in/components/graphs/PackingGraph.js
@@ -9,7 +9,6 @@ import { mouseOver, mouseMove, mouseLeave } from './mouseEvents'
 const styles = theme => ({
   tooltip: {
     borderColor: theme.palette.common.alternative,
-    // position: "absolute",
   }
 })
 
@@ -43,35 +42,6 @@ function PackingGraph(props) {
       startingOpacity: 0,
     })
 
-    // const displayedTooltip = d3.select("#PackingGraph")
-    //   .append("div")
-    //   .style("opacity", 0)
-    //   .style("position", "absolute")
-    //   .style("background-color", "white")
-    //   .style("border", "solid")
-    //   .style("border-width", "1px")
-    //   .style("border-radius", "5px")
-    //   .style("padding", "7px")
-  
-    // // Three function that change the tooltip when user hover / move / leave a cell
-    // const mouseover = function(event, d) {
-    //   console.log('yo')
-    //   graphTooltip
-    //     .style("opacity", 0.97)
-    // }
-    // const mousemove = function(event, d) {
-    //   graphTooltip
-    //     .html(d.description + "<br>" + "Eaten "+ d.value + " times")
-    //     .style("left", (d.x) + "px")
-    //     .style("top", (event.screenY) + "px")
-    //     .style("width", "max-Content")
-    //     .style("opacity", 0.97)
-    // }
-    // const mouseleave = function(d) {
-    //   graphTooltip
-    //     .style("opacity", 0)
-    // }
-    
     const node = svg
       .selectAll("circle")
       .data(data)
@@ -85,6 +55,7 @@ function PackingGraph(props) {
         .style("fill-opacity", 0.8)
         .attr("stroke", "black")
         .style("stroke-width", 1)
+        .style("cursor", "grab")
         .on("mouseover", (event, d) => {
           mouseOver(displayedTooltip)
         })
@@ -95,10 +66,10 @@ function PackingGraph(props) {
         .on("mouseleave",(event, d) => {
           mouseLeave(displayedTooltip)
         })
-        // .call(d3.drag() // call specific function when circle is dragged
-        //      .on("start", dragstarted)
-        //      .on("drag", dragged)
-        //      .on("end", dragended));
+        .call(d3.drag() // call specific function when circle is dragged
+             .on("start", dragstarted)
+             .on("drag", dragged)
+             .on("end", dragended));
   
     const simulation = d3.forceSimulation()
       .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
@@ -113,20 +84,20 @@ function PackingGraph(props) {
             .attr("cy", function(d){ return d.y; })
       });
 
-    // function dragstarted(d) {
-    //   if (!d3.event.active) simulation.alphaTarget(.03).restart();
-    //   d.fx = d.x;
-    //   d.fy = d.y;
-    // }
-    // function dragged(d) {
-    //   d.fx = d3.event.x;
-    //   d.fy = d3.event.y;
-    // }
-    // function dragended(d) {
-    //   if (!d3.event.active) simulation.alphaTarget(.03);
-    //   d.fx = null;
-    //   d.fy = null;
-    // }
+    function dragstarted(event, d) {
+      if (!event.active) simulation.alphaTarget(.03).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+    function dragged(event, d) {
+      d.fx = event.x;
+      d.fy = event.y;
+    }
+    function dragended(event, d) {
+      if (!event.active) simulation.alphaTarget(.03);
+      d.fx = null;
+      d.fy = null;
+    }
 
   })
 
