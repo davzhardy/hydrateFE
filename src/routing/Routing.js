3afe9b2f56a7e7f617af7e4d4b668ed833b1b340
David Hardy <david.hardy50@gmail.com>
2021-02-14 22:19:16 +0100

    route refactoring

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -7,31 +7,6 @@ const Router = () => {
   const LoggedInComponent = lazy(() => import("../logged_in/components/Main"));
   const LoggedOutComponent = lazy(() => import("../logged_out/components/Main"));
 
-  const [routes, setRoutes] = useState([]);
-
-  // useEffect(() => {
-  //   importRoutes();
-  // },[]);
-
-  // const importRoutes = async () => {
-  //   const importPromises = routesDefined.map((item) =>
-  //     import(`../../containers/${item.component}`).then((module) =>
-  //       setRoutes((routes) => [
-  //         ...routes,
-  //         {
-  //           to: item.to,
-  //           title: item.title,
-  //           isPrivate: item.isPrivate,
-  //           exact: item.exact,
-  //           Component: module.default,
-  //         },
-  //       ])
-  //     )
-  //   );
-  //   await Promise.all(importPromises);
-  // };
-  
-
   return (
     <Switch>
       <Route path="/loggedIn">
@@ -41,35 +16,6 @@ const Router = () => {
         <LoggedOutComponent />
       </Route>
     </Switch>
-    // <Switch>
-    //   {routes?.map((route) => {
-    //     if (route.isPrivate) {
-    //       return (
-    //         <PrivateRoute
-    //           key={`${route.to}-${route.title}`}
-    //           path={route.to}
-    //           exact={route.exact}
-    //           render={() => (
-    //             <route.Component />
-    //           )}
-    //         />
-    //       );
-    //     } else if (!route.isPrivate) {
-    //       return (
-    //         <Route
-    //           key={`${route.to}-${route.title}`}
-    //           path={route.to}
-    //           render={() => (
-    //             <Layout >
-    //               <route.Component />
-    //             </Layout>
-    //           )}
-    //         />
-    //       );
-    //     }
-    //   })
-    // }
-    // </Switch>
   );
 };
 
