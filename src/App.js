914a779b05331adb6a07d61c3678deb14bdf966f
David Hardy <david.hardy50@gmail.com>
2021-03-05 11:09:01 +0100

    chore: minor changes

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import PrivateRoute from './shared/PrivateRoute'
 import ReactGA from 'react-ga';
 
 ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID || undefined);
-console.log(process.env.NODE_ENV)
-console.log(process.env.REACT_APP_ENDPOINT)
 
 const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
 const LoggedOutComponent = lazy(() => import("./logged_out/components/Main"));
